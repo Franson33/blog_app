a29d5f696dd1b72678289a8c820ce11f52944794
Franson33 <antonchikfr@gmail.com>
2025-10-04 19:16:30 +0300

    refactored the content module, replace hardcoded list with fetched one

--- lib/blog_app/content.ex
+++ lib/blog_app/content.ex
@@ -3,11 +3,23 @@ defmodule BlogApp.Content do
   Fetches and parses articles stored in GitHub repo.
   """
 
+  alias HTTPoison.Response
+
+  @base_url "https://raw.githubusercontent.com/Franson33/articles/main"
+  @manifest_url "#{@base_url}/manifest.json"
+
   def fetch_article(slug) do
     slug
     |> raw_url()
     |> fetch_markdown()
     |> parse_markdown()
+  end
+
+  def list_articles do
+    case fetch_manifest() do
+      {:ok, articles} -> {:ok, articles}
+      {:error, _} -> {:error, :invalid_json}
+    end
   end
 
   defp fetch_markdown(url) do
@@ -29,20 +41,23 @@ defmodule BlogApp.Content do
   end
 
   defp raw_url(slug) do
-    "https://raw.githubusercontent.com/Franson33/articles/main/articles/#{slug}.md"
+    "#{@base_url}/articles/#{slug}.md"
   end
 
-  def list_articles() do
-    [
-      %{
-        slug: "i-love-state-machines",
-        title: "I love finite-state machines"
-      },
-      %{
-        slug: "closure-based-architecture",
-        title:
-          "React's Component Revolution: How Closures Became the Foundation of Modern UI Components"
-      }
-    ]
+  defp fetch_manifest do
+    @manifest_url
+    |> HTTPoison.get()
+    |> handle_response()
+  end
+
+  defp handle_response({:ok, %Response{status_code: 200, body: json}}), do: decode_json(json)
+  defp handle_response({:ok, %Response{status_code: code}}), do: {:error, {:http_error, code}}
+  defp handle_response({:error, reason}), do: {:error, {:network_failure, reason}}
+
+  defp decode_json(json) do
+    case Jason.decode(json) do
+      {:ok, articles} -> {:ok, articles}
+      {:error, _} -> {:error, :invalid_json}
+    end
   end
 end
